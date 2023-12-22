5599ade3b5fbbe876aa0db270e5b227cd2f2d2ad
Dragonfly0511 <tammy.moberg@gmail.com>
2023-12-22 16:55:23 -0600

    added prompts and confirms to js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 // Assignment code here
 
+const str1 = 'abcdefghijklmnopqrstuvwxyz'; 
+const str2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const str3 = '0123456789'
+const str4 = '!@#$%^&*()-_=+[]{}|;:,.<>?'
+const password = 'str1 + str2 + str3 + str4';
+
+window.prompt("How many characters would you like to be included")
+
+window.confirm("Would you like it to include lowercase letters?")
+
+window.confirm("Would you like it to include special characters?")
+
+
+
+console.log(str1 + str2 + str3 + str4)
+
+for(var i=0; i < str1.length; i++) [ console.log('Here is your password, ' + password[i])]
+
+
+
+var generatePassword= function() {
+  console.log(Math.floor(Math.random() * 128));
+  return;
+}
+
+generatePassword()
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -27,28 +53,15 @@ generateBtn.addEventListener("click", writePassword);
 //
   //define what the user wants for their password using PROMPT for open ended questions
   //Prompts ("How many characters would you like to be included") 
+    //make random x length?
   //Confirm ("Would you like it to include lowercase letters?")
+    //if yes include, if no don't include
   //Confirm ("Would you like it to include special characters?")
+    //if yes include, if no, don't include
 //Once criteria set, a password needs to be generated that is randomized
+  //but how?
 
 //make potential character sets
 //global variables at the top of page to be defined first
 //define the users input based off of their prompts and confirms?
 //Make alert to display password
-
-const str1 = 'abcdefghijklmnopqrstuvwxyz';
-const str2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const str3 = '0123456789'
-const str4 = '!@#$%^&*()-_=+[]{}|;:,.<>?'
-
-
-console.log(str1 + str2 + str3 + str4)
-
-
-
-var generatePassword= function() {
-  console.log(Math.floor(Math.random() * 128));
-  return;
-}
-
-generatePassword()
